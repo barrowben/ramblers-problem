1a06027ed1cc8b94b3b0f2ba98218c377a5760a6
Ben Barrow <babarrow1@sheffield.ac.uk>
2021-05-16 21:19:29 +0100

    add logging of console output

--- search4/RunRamblersBB.java
+++ search4/RunRamblersBB.java
@@ -8,33 +8,34 @@
 */
 
 import java.util.*;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 public class RunRamblersBB {
   public static void main(String[] arg) {
 
     TerrainMap tMap = new TerrainMap("tmc.pgm");
 
-    RamblersSearch rSearcher = new RamblersSearch(tMap, 15 , 15); // value = 150
+    RamblersSearch rSearcher = new RamblersSearch(tMap, 13 , 11); // value = 150
     SearchState initState = new RamblersState(150, 0, 0, 0);
+    System.out.println(initState);
 
     String res_bb = rSearcher.runSearch(initState, "branchAndBound");
     System.out.println(res_bb);
-    // String[] USACities = { "Albany", "Atlanta", "Augusta", "Austin", "Bismarck", "Boise", "Boston", "Chicago", "Dallas",
-    //     "Denver", "Detroit", "Helena", "Indianapolis", "Jefferson City", "Las Vegas", "Los Angeles", "Memphis", "Miami",
-    //     "New Orleans", "New York", "Oklahoma City", "Phoenix", "Pierre", "Salem", "Salt Lake City", "San Francisco",
-    //     "Santa Fe", "Seattle", "Tallahassee", "Washington DC" };
 
-    // System.out.println(mapUSA.toString());
-    // System.out.println(mapUSA.getLinks("Start"));
-
-    // for (String city: USACities) {
-    //         MapSearch searcher = new MapSearch(mapUSA, city);
-    //         SearchState initState = (SearchState) new MapState("Denver", 0, 0);
-
-    //         String res_bb = searcher.runSearch(initState, "branchAndBound");
-    //         // System.out.println("Start: Denver | Target: " + city);
-    //         System.out.println(res_bb);
-    // }
-    // add your solution code here ....
+    // Log the console output to a textfile
+    try {
+        FileWriter file = new FileWriter("output.txt");
+        PrintWriter output = new PrintWriter(file, true);
+        output.append("=================");
+        output.append("START EXPERIMENT");
+        output.append("=================");
+        output.append("\n");
+        output.append(res_bb);
+        output.append("\n");
+        output.close();
+    } catch (Exception e) {
+        e.getStackTrace();
+    }
   }
 }
