a802b8113097802b4c50cab304c65781eb30ea6d
Ben Barrow <babarrow1@sheffield.ac.uk>
2021-05-17 00:53:03 +0100

    log results to different csv depending on search strat

--- search4/RunRamblersBB.java
+++ search4/RunRamblersBB.java
@@ -14,8 +14,21 @@ import java.io.PrintWriter;
 public class RunRamblersBB {
   public static void main(String[] arg) {
 
+    PrintWriter output = null;
+    String strategy = "branchAndBound";
+
     TerrainMap tMap = new TerrainMap("tmc.pgm");
 
+      try {
+          FileWriter file = new FileWriter(".log/" + strategy + "_output.csv", true);
+          output = new PrintWriter(file, true);
+          output.append("Start Y,Start X,Goal Y,Goal X,Efficiency");
+          output.close();
+      } catch (Exception e) {
+          e.getStackTrace();
+      }
+
+    // Run the experiment 5 times with random start and goal nodes
     for (int i = 0; i<5; i++) {     
         int min = 0;
         int max = tMap.getWidth();
@@ -29,23 +42,23 @@ public class RunRamblersBB {
         RamblersSearch rSearcher = new RamblersSearch(tMap, randomYGoal , randomXGoal);
 
         SearchState initState = new RamblersState(initialHeight, randomYOrigin, randomXOrigin, 0);
-        System.out.println(initState);
+        // System.out.println(initState);
 
-        String res_bb = rSearcher.runSearch(initState, "branchAndBound");
+        float res_bb = rSearcher.runSearchE(initState, strategy);
         System.out.println(res_bb);
 
-        String fileName = ".log/output_" + randomYOrigin + "-" + randomXOrigin  + "_" + randomYGoal  + "-" + randomXGoal + ".txt";
+        // String fileName = ".log/output_" + randomYOrigin + "-" + randomXOrigin  + "_" + randomYGoal  + "-" + randomXGoal + ".csv";
 
         // Log the console output to a textfile
         try {
-            FileWriter file = new FileWriter(fileName);
-            PrintWriter output = new PrintWriter(file, true);
-            output.append("=================");
-            output.append("START EXPERIMENT");
-            output.append("=================");
+            FileWriter file = new FileWriter(".log/" + strategy + "_output.csv", true);
+            output = new PrintWriter(file, true);
             output.append("\n");
-            output.append(res_bb);
-            output.append("\n");
+            output.append(randomYOrigin + ",");
+            output.append(randomXOrigin + ",");
+            output.append(randomYGoal + ",");
+            output.append(randomXGoal + ",");
+            output.append(String.valueOf(res_bb));
             output.close();
         } catch (Exception e) {
             e.getStackTrace();
