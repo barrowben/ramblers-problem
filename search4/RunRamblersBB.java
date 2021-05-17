5c3ec9828b23401d88e0a4768647857caa6d803d
Ben Barrow <babarrow1@sheffield.ac.uk>
2021-05-17 01:08:38 +0100

    improvements to CSV output, default no. experiments = 100

--- search4/RunRamblersBB.java
+++ search4/RunRamblersBB.java
@@ -14,42 +14,44 @@ import java.io.PrintWriter;
 public class RunRamblersBB {
   public static void main(String[] arg) {
 
+    // Create PrintWrite obj for output
     PrintWriter output = null;
+
+    // Change the strategy here
     String strategy = "branchAndBound";
 
+    // Create TerrainMap obj
     TerrainMap tMap = new TerrainMap("tmc.pgm");
 
-      try {
-          FileWriter file = new FileWriter(".log/" + strategy + "_output.csv", true);
-          output = new PrintWriter(file, true);
-          output.append("Start Y,Start X,Goal Y,Goal X,Efficiency");
-          output.close();
-      } catch (Exception e) {
-          e.getStackTrace();
-      }
+    // Write the csv column headers
+    try {
+        FileWriter file = new FileWriter(".log/" + strategy + "_output.csv", true);
+        output = new PrintWriter(file, true);
+        output.append("Start Y,Start X,Goal Y,Goal X,Efficiency");
+        output.close();
+    } catch (Exception e) {
+        System.out.println(e.getStackTrace());
+    }
 
-    // Run the experiment 5 times with random start and goal nodes
-    for (int i = 0; i<5; i++) {     
-        int min = 0;
-        int max = tMap.getWidth();
+    // Run the experiment 100 times with random start and goal nodes
+    for (int i = 0; i<100; i++) {     
+        int max = tMap.getWidth(); // assuming the pgm is square
         Random random = new Random();
-        int randomYOrigin = random.nextInt(max + min);
-        int randomXOrigin = random.nextInt(max + min);
-        int randomYGoal = random.nextInt(max + min);
-        int randomXGoal = random.nextInt(max + min);
+
+        //randomise the start and goal nodes and get initial height
+        int randomYOrigin = random.nextInt(max);
+        int randomXOrigin = random.nextInt(max);
+        int randomYGoal = random.nextInt(max);
+        int randomXGoal = random.nextInt(max);
         int initialHeight = tMap.getHeight();
 
+        // Start the search
         RamblersSearch rSearcher = new RamblersSearch(tMap, randomYGoal , randomXGoal);
-
         SearchState initState = new RamblersState(initialHeight, randomYOrigin, randomXOrigin, 0);
-        // System.out.println(initState);
-
         float res_bb = rSearcher.runSearchE(initState, strategy);
         System.out.println(res_bb);
 
-        // String fileName = ".log/output_" + randomYOrigin + "-" + randomXOrigin  + "_" + randomYGoal  + "-" + randomXGoal + ".csv";
-
-        // Log the console output to a textfile
+        // Log the output to a textfile
         try {
             FileWriter file = new FileWriter(".log/" + strategy + "_output.csv", true);
             output = new PrintWriter(file, true);
@@ -61,7 +63,7 @@ public class RunRamblersBB {
             output.append(String.valueOf(res_bb));
             output.close();
         } catch (Exception e) {
-            e.getStackTrace();
+            System.out.println(e.getStackTrace());
         }
     }
   }
